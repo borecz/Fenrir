5c86df02fb5ff7d1a92161845ed7e05df67be6bd
Simone Borelli <simone.borelli@aboutyou.com>
2022-08-03 10:33:14 +0200

    scaffold(e2e): setup basic project

---
+++ cypress.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'cypress';
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on: any, config: any) {
+      // implement node event listeners here
+    },
+  },
+});
